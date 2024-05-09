98605db049396f53b7c1271c13074e06db4dfbdb
Akbar Rahmat Mulyatama <88664674+akbarrahmatm@users.noreply.github.com>
2024-05-09 18:42:53 +0700

    Fix : Fixing null bug in Title

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -3,9 +3,7 @@ import { Helmet, HelmetProvider } from "react-helmet-async";
 export default function Title({ page }) {
   return (
     <HelmetProvider>
-      <Helmet>
-        <title>{page} - Binar Car Rental</title>
-      </Helmet>
+      <Helmet>{page && <title>{page} - Binar Car Rental</title>} </Helmet>
     </HelmetProvider>
   );
 }
